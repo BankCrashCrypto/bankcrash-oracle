1e207931db901fa1c6bf916751c7e2a0a3742335
Sato Cvik <sato@bankcrash.gg>
2023-06-27 15:59:41 +0200

    write to blockchain tries

--- test.py
+++ test.py
@@ -1,41 +1,44 @@
 #%%
 import config_test_acc
 from web3 import Web3
+import json
 
 infura_url = config_test_acc.infura_url
 w3 = Web3(Web3.HTTPProvider(infura_url))
 
-#%%
-import json
-
 contract_address = config_test_acc.contract_address
 with open(config_test_acc.contract_abi_file, "r") as f:
-	contract_abi = json.load(f)
+	contract_abi = json.load(f)["abi"]
 
-# Create contract instance
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
 
 def write_to_blockchain(is_small_bank_crash, is_middle_bank_crash, is_lage_bank_crash):
-    # replace 'YOUR_ACCOUNT_ADDRESS' and 'YOUR_PRIVATE_KEY' with your Ethereum account address and private key
-    nonce = w3.eth.getTransactionCount(config_test_acc.account_address)
+    nonce = w3.eth.get_transaction_count(config_test_acc.account_address)
 
-		# function_to_call=updateBankCrashEvents(bool bigBankCrashEvent, bool mediumBankCrashEvent, bool smallBankCrashEvent)
-
-    txn_dict = contract.functions.updateBankCrashEvents(is_lage_bank_crash, is_middle_bank_crash, is_small_bank_crash).buildTransaction({
+    txn_dict = contract.functions.updateBankCrashEvents(
+      is_lage_bank_crash, is_middle_bank_crash, is_small_bank_crash
+      ).build_transaction({
         'chainId': 1,
-        'gas': 70000,
-        'gasPrice': w3.toWei('1', 'gwei'),
+        'gas': 700000,
+        'gasPrice': w3.to_wei('1', 'gwei'),
+        # 'maxFeePerGas': w3.to_wei('2', 'gwei'),
+        # 'maxPriorityFeePerGas': w3.to_wei('1', 'gwei'),
         'nonce': nonce,
     })
 
-    signed_txn = w3.eth.account.signTransaction(txn_dict, private_key=config_test_acc.account_private_key)
-
-    result = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-    tx_receipt = w3.eth.waitForTransactionReceipt(result)
+    signed_txn = w3.eth.account.sign_transaction(txn_dict, private_key=config_test_acc.account_private_key)
+    # rr = w3.to_hex(w3.keccak(signed_txn.rawTransaction))
+    
+    result = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    print("progressing")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(result)
+    print("DONE!")
 
     print(tx_receipt)
 
+write_to_blockchain(True, False, False)
 # %%
+# %%
+
 # %%
