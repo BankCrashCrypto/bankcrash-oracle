2a08f362a3cec423da9fea09b0bc480998458f83
Sato Cvik <sato@bankcrash.gg>
2023-07-19 16:15:22 +0200

    issues with the yahoo currencies... not a big deal... really... WTF :D

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git reset --hard
-git pull origin main
-sed -i '1d;2d;3d' oracle.py
+ssh -i "~/.ssh/hm_notebook.pem" ubuntu@api.bankcrash.gg '(cd bankcrash-oracle && git reset --hard && git pull origin main)'
+ssh -i "~/.ssh/hm_notebook.pem" ubuntu@api.bankcrash.gg 'rm ~/bankcrash-oracle/config_accounts.py'
+scp -i "~/.ssh/hm_notebook.pem" ~/repo/bankcrash-oracle/config_accounts.py ubuntu@api.bankcrash.gg:~/bankcrash-oracle
